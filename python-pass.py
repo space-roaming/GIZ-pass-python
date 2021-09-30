0f0d942d7bcafbc7f93b338b9acbc6c3c4a32624
space-roaming <space.roaming27@gmail.com>
2021-09-30 14:02:03 +0300

    sol

--- python-pass.py
+++ python-pass.py
@@ -1,7 +1,37 @@
+def printSubStr(str, low, high):
+    for i in range(low, high + 1):
+        print(str[i], end="")
 
 
-class Solution:
-
+class solution:
     @staticmethod
-    def longest_palindromic(s: str) -> str:
+    def longest_palindromic(s : str)-> str:
+        n = len(s)
+
+        maxLength = 1
+        start = 0
+
+        for i in range(n):
+            for j in range(i, n):
+                flag = 1
+
+                # Check palindrome
+                for k in range(0, ((j - i) // 2) + 1):
+                    if (s[i + k] != s[j - k]):
+                        flag = 0
+
+                # Palindrome
+                if (flag != 0 and (j - i + 1) > maxLength):
+                    start = i
+                    maxLength = j - i + 1
+
+        printSubStr(s, start, start + maxLength - 1)
+        return str(maxLength)
+
         pass
+
+
+ print("\nLength is: ",solution.longest_palindromic("babad"))
+    print("\nLength is: ",solution.longest_palindromic("cbbd"))
+    print("\nLength is: ",solution.longest_palindromic("a"))
+    print("\nLength is: ",solution.longest_palindromic("ac"))
